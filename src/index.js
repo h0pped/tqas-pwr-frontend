08112538a655a6cb9ddc5d7039844ba09b65e635
h0pped <notawril@gmail.com>
2022-10-06 18:18:23 +0200

    add base layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import Login from './pages/Login/Login.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import classes from './Login.module.css';
+
+const Login = () => (
+  <div className={classes.container}>
+    <h1>Login</h1>
+  </div>
+);
+
+export default Login;
